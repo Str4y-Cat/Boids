883d1d74cd4d036cc446d1221d2a13d0e8df7e3f
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-07-25 11:45:12 +0200

    refactored further. Added comments and descriptions. Removed unused notes

--- Boids 3d/src/boidScripts/BoidController.js
+++ Boids 3d/src/boidScripts/BoidController.js
@@ -1,17 +1,9 @@
 import BoidLogic from "./BoidLogic";
 import boidConfig from "./boid.config";
-import Performance from "../performance/Performance";
 import * as THREE from 'three'
 
 export default class BoidController
 {
-
-    /**
-     *  
-     * 
-     * 
-     * 
-     */
 
     /** constructor()
      * 
@@ -29,17 +21,28 @@ export default class BoidController
         this.boidMeshes= []
         this.boidCount=null
         
-        this.setBoidLogic(count)
-
-        this.performance= new Performance()
+        this.#setBoidLogic(count)
     }
 
     //#region boids
-    setBoidLogic(count)
+
+    /**
+     * inititate the boid logic
+     * 
+     * @param {int} count 
+     */
+    #setBoidLogic(count)
     {
         this.boidLogic=new BoidLogic(count, this.boundingBox)
     }
 
+    /**
+     * Sets a native geometry mesh for each boid
+     * 
+     * @param {*} geometry - THREE.js geometry
+     * @param {*} material - THREE.js material
+     * @param {bool} rotateX 
+     */
     setStandardMesh(geometry,material,rotateX=true)
     {
         //set global geometry
@@ -51,13 +54,20 @@ export default class BoidController
             geometry.rotateX(-Math.PI * 0.5);
         }
 
-        //addMeshes()
-        this.addMeshes(this.boidLogic.boidArray)
+        //#addMeshes()
+        this.#addMeshes(this.boidLogic.boidArray)
     }
 
-    addMeshes(arr)
+    /**
+     * Adds all positions in array to mesh array and add to scene
+     * 
+     * @param {*} boidLogicArray 
+     * @returns 
+     */
+    #addMeshes(boidLogicArray)
     {
 
+        //check if mat and geo has been set
         if(!this.material)
         {
             console.log('No material has been set')
@@ -69,11 +79,12 @@ export default class BoidController
             return
         }
         
-       
-        arr.forEach((boid) => {
-            this.createMesh(boid)
+       //create mesh for each boid position
+        boidLogicArray.forEach((boid) => {
+            this.#createMesh(boid)
         });
 
+        //update boidcount
         this.boidCount=this.boidMeshes.length
         
         
@@ -81,46 +92,74 @@ export default class BoidController
  
     }
 
-    createMesh({x,y,z})
+    /**
+     * creates the three.js mesh for boidLogic object
+     * 
+     * @param {object} boidPosition 
+     */
+    #createMesh({x,y,z})
     {
+        //create mesh
         const boidMesh= new THREE.Mesh(this.geometry,this.material)
+        //set position of mesh
         boidMesh.position.set(x,y,z)
+
+        //add to scene
         this.scene.add(boidMesh)
 
+        //push to array of boid meshes
         this.boidMeshes.push(boidMesh)
     }
 
-    removeMesh()
+    /**
+     * removes last mesh in boidmesh array
+     */
+    #removeMesh()
     {
+        //remove last mesh from array
         const mesh= this.boidMeshes.pop()
 
+        //remove from scene and free up memory
         this.scene.remove(mesh)
         mesh.geometry.dispose()
         mesh.material.dispose()     
     }
 
-    addBoids(count)
+    /**
+     * 
+     * @param {int} count 
+     */
+    #addBoids(count)
     {
+        //add new boids to boidLogic instance
         this.boidLogic.addBoids(count)
+
+        //instanciate start and end index values
         const iStart=this.boidLogic.boidArray.length-count
         const iEnd= this.boidLogic.boidArray.length
 
+        //create mesh based on each new boid added
         for(let i= iStart; i<iEnd;i++){
-            this.createMesh([this.boidLogic.boidArray[i]])
+            this.#createMesh([this.boidLogic.boidArray[i]])
         }
     }
 
-    removeBoids(count)
+    /**
+     * 
+     * @param {int} count 
+     */
+    #removeBoids(count)
     {
-        // console.log(`Removing-----------------`)
-        // console.log(`count is ${count}\nBefore pos: ${this.boidLogic.boidArray.length}\nBefore mesh: ${this.boidMeshes.length}`)
-
+        //remove boids from boid logic instance
         this.boidLogic.removeBoids(count)
 
+        //instanciate start and end indicies
         const iStart=this.boidLogic.boidArray.length
         const iEnd= this.boidLogic.boidArray.length+count
+
+        //remove meshes
         for(let i=iStart; i<iEnd;i++){
-            this.removeMesh()
+            this.#removeMesh()
         }
         // console.log(`After pos: ${this.boidLogic.boidArray.length}\nAfter mesh: ${this.boidMeshes.length}`)
 
@@ -131,53 +170,55 @@ export default class BoidController
     //#endregion
 
     //#region utils
-    
-    //#endregion
 
-    /** Update()
+    /**
      * 
-     * Updates the movement of the boid objects
-     * 
+     * @param {[obj]} environmenObjects 
      */
     update(environmenObjects)
     {
-        this.performance.timer('boidLogic',true)
+        //update the logic
         this.boidLogic.update(environmenObjects)
-        this.performance.timer('boidLogic',true)
 
-        // this.performance.timer('boid Draw')
+        //update the meshes
         this.boidMeshes.forEach((boidMesh,i)=>
         {
             const boid= this.boidLogic.boidArray[i]
 
             boidMesh.position.copy(boid)
            
+            //rotate correctly
             boidMesh.lookAt(new THREE.Vector3(boid.targetX,boid.targetY,boid.targetZ))
         })
-        // this.performance.timer('boid Draw')
 
+        //check if debug is active
         if(this.debug)
-        {
+        {   
+            //show protected range visualization
             if(this.debug.protectedRange)
             {
                 this.debug.protectedRange.position.copy(this.boidMeshes[0].position)
             }
+
+            //show visual range visualization
             if(this.debug.visualRange)
                 {
                     this.debug.visualRange.position.copy(this.boidMeshes[0].position)
                 }
         }
-
-        // if(this.followBoid)
-        // {
-        //     // const position= this.boidMeshes[this.followBoid.index].position.clone()
-        //     // this.camera.position.copy(position.multiplyScalar(2))
-        //     this.camera.lookAt(this.boidMeshes[this.followBoid.index].position)
-        // }
     }
+
+    //#endregion
+
+    
 
 
     //#region DEBUG
+    /**
+     * Set up the debug panel
+     * 
+     * @param {*} gui lil-gui instance
+     */
     viewDebug(gui)
     {
         this.debug= {}
@@ -189,52 +230,53 @@ export default class BoidController
 
         
         //add count tweak
-        this.debugCount()
+        this.#debugCount()
         //parameters tweaks
-        this.debugValues()
-
-        this.debugSolidBorderBox()
-
-        this.debugVisionRange()
+        this.#debugValues()
+        //bounding box visualization
+        this.#debugSolidBorderBox()
+        //boid vision visualization
+        this.#debugVisionRange()
     }
 
-    /** addControls
-     * 
-     * adds gui controls
-     * 
+    /** 
+     * Setup boid Count tweak
      */
-    debugCount()
-    {
+    #debugCount()
+    {   
         this.debug.folder.add(this.debug,'boidCount'). min(1).max(1500).step(1).onChange((count)=>
         {
             this.boidCount=count
             if(count>this.boidMeshes.length)
                 {
-                    this.addBoids(count-this.boidMeshes.length)
+                    this.#addBoids(count-this.boidMeshes.length)
                 }
             if(count<this.boidMeshes.length)
                 {
-                    this.removeBoids(this.boidMeshes.length-count)
+                    this.#removeBoids(this.boidMeshes.length-count)
                 }
         })
 
     }
 
-    debugValues()
+    /** 
+     * Setup boid values tweaks
+     */
+    #debugValues()
     {
         
         this.debug.folder.add(boidConfig.values,"visualRange").min(0.5).max(3).step(0.00001).onChange((num)=>{
             this.boidLogic.visualRange=num
             if(this.debug.visualRange)
                 {
-                    this.debugVisualRange(true)
+                    this.#debugVisualRange(true)
                 }
         })
         this.debug.folder.add(boidConfig.values,"protectedRange").min(0.1).max(2).step(0.00001).onChange((num)=>{
             this.boidLogic.protectedRange=num
             if(this.debug.protectedRange)
             {
-                this.debugProtectedRange(true)
+                this.#debugProtectedRange(true)
             }
         })
         this.debug.folder.add(boidConfig.values,"objectAvoidFactor").min(0).max(10).step(0.00001).onChange((num)=>{
@@ -262,8 +304,10 @@ export default class BoidController
         
     }
 
-    //debug border box
-    debugSolidBorderBox()
+    /** 
+     * set up border box tweak
+     */
+    #debugSolidBorderBox()
     {
         this.debug.showBoundingBox=false
         this.debug.folder.add(this.debug,'showBoundingBox').onChange(()=>
@@ -282,7 +326,10 @@ export default class BoidController
         
     }
 
-    debugProtectedRange(needsUpdate=false)
+    /** 
+     * Setup protected range visualization
+     */
+    #debugProtectedRange(needsUpdate=false)
     {
         if(!needsUpdate)
         {
@@ -313,7 +360,10 @@ export default class BoidController
         
     }
 
-    debugVisualRange(needsUpdate=false)
+    /** 
+     * Setup visual range visualization
+     */
+    #debugVisualRange(needsUpdate=false)
     {
         if(!needsUpdate)
         {
@@ -344,7 +394,10 @@ export default class BoidController
         
     }
 
-    debugVisionRange()
+    /** 
+     * Setup vision teak
+     */
+    #debugVisionRange()
     {
         this.debug.showProtectedRange=false
         this.debug.showVisualRange=false
@@ -361,7 +414,7 @@ export default class BoidController
             else
             {
                 
-                this.debugProtectedRange()
+                this.#debugProtectedRange()
                 
             }
         })
@@ -377,7 +430,7 @@ export default class BoidController
                 }
                 else
                 {
-                    this.debugVisualRange()
+                    this.#debugVisualRange()
                 }
             })
         
