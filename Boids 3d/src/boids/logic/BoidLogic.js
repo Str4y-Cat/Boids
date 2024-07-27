6e0283e483d5c4ba4cd7d64269dfc75112196e9f
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-07-27 18:56:11 +0200

    now store rotation within the logic array. uses three.js classes to hold data

--- Boids 3d/src/boids/logic/BoidLogic.js
+++ Boids 3d/src/boids/logic/BoidLogic.js
@@ -1,5 +1,7 @@
 
 import boidConfig from "../boid.config";
+import * as THREE from 'three'
+
 export default class BoidLogic
 {
     /**
@@ -98,9 +100,9 @@ export default class BoidLogic
             {
 
                 //set up the rotation target
-                boid.targetX=boid.x
-                boid.targetY=boid.y
-                boid.targetZ=boid.z
+                // boid.targetX=boid.x
+                // boid.targetY=boid.y
+                // boid.targetZ=boid.z
 
                 //zero accum variables
                 let accum= this.#accumulatorObject()
@@ -109,9 +111,9 @@ export default class BoidLogic
                 this.boidArray.forEach((otherBoid,n)=>
                     {
                         //compute differences in xy coords
-                        const dx= boid.x - otherBoid.x
-                        const dy= boid.y - otherBoid.y
-                        const dz= boid.z - otherBoid.z
+                        const dx= boid.position.x - otherBoid.position.x
+                        const dy= boid.position.y - otherBoid.position.y
+                        const dz= boid.position.z - otherBoid.position.z
 
                         //check if they are within visual range
                         if(Math.abs(dx)<this.visualRange && Math.abs(dy)<this.visualRange&& Math.abs(dz)<this.visualRange)
@@ -136,12 +138,12 @@ export default class BoidLogic
                                     {
                                         const exp=(1-(distanceSquared/VISUAL_RANGE_SQUARED))**2
                                         //add other boids x/y coords and velocity variables to the accum
-                                        accum.xpos_avg+=otherBoid.x
-                                        accum.ypos_avg+=otherBoid.y
-                                        accum.zpos_avg+=otherBoid.z
-                                        accum.xvel_avg+=otherBoid.vx*exp
-                                        accum.yvel_avg+=otherBoid.vy*exp
-                                        accum.zvel_avg+=otherBoid.vz*exp
+                                        accum.xpos_avg+=otherBoid.position.x
+                                        accum.ypos_avg+=otherBoid.position.y
+                                        accum.zpos_avg+=otherBoid.position.z
+                                        accum.xvel_avg+=otherBoid.velocity.x*exp
+                                        accum.yvel_avg+=otherBoid.velocity.y*exp
+                                        accum.zvel_avg+=otherBoid.velocity.z*exp
 
                                         //increment number of boids in visual range
                                         accum.neighboring_boids++
@@ -167,25 +169,25 @@ export default class BoidLogic
 
                         
                         //add cohesion and alignment factors
-                        boid.vx+= (accum.xpos_avg-boid.x)*this.cohesionFactor
-                        boid.vx+= (accum.xvel_avg-boid.vx)*this.matchingFactor
+                        boid.velocity.x+= (accum.xpos_avg-boid.position.x)*this.cohesionFactor
+                        boid.velocity.x+= (accum.xvel_avg-boid.velocity.x)*this.matchingFactor
                         // console.log('cohesion factor',(accum.xpos_avg-boid.x)*this.cohesionFactor)
                         // console.log('matching factor',(accum.xvel_avg-boid.vx)*this.matchingFactor)
 
-                        boid.vy+= (accum.ypos_avg-boid.y)*this.cohesionFactor
-                        boid.vy+= (accum.yvel_avg-boid.vy)*this.matchingFactor
+                        boid.velocity.y+= (accum.ypos_avg-boid.position.y)*this.cohesionFactor
+                        boid.velocity.y+= (accum.yvel_avg-boid.velocity.y)*this.matchingFactor
 
-                        boid.vz+= (accum.zpos_avg-boid.z)*this.cohesionFactor
-                        boid.vz+= (accum.zvel_avg-boid.vz)*this.matchingFactor
+                        boid.velocity.z+= (accum.zpos_avg-boid.position.z)*this.cohesionFactor
+                        boid.velocity.z+= (accum.zvel_avg-boid.velocity.z)*this.matchingFactor
 
                         
             
                     }
 
                     //Add sepperation factor
-                    boid.vx+= (accum.close_dx*this.seperationFactor)
-                    boid.vy+= (accum.close_dy*this.seperationFactor)
-                    boid.vz+= (accum.close_dz*this.seperationFactor)
+                    boid.velocity.x+= (accum.close_dx*this.seperationFactor)
+                    boid.velocity.y+= (accum.close_dy*this.seperationFactor)
+                    boid.velocity.z+= (accum.close_dz*this.seperationFactor)
                 }
 
                 //there are other objects! get out of the way
@@ -195,13 +197,13 @@ export default class BoidLogic
                     //avoiding object
                     const avoidObjExp=(1-environmentObjects[i].distance)**3
 
-                    const dx= boid.x - environmentObjects[i].position.x
-                    const dy= boid.y - environmentObjects[i].position.y
-                    const dz= boid.z - environmentObjects[i].position.z
+                    const dx= boid.position.x - environmentObjects[i].position.x
+                    const dy= boid.position.y - environmentObjects[i].position.y
+                    const dz= boid.position.z - environmentObjects[i].position.z
 
-                    boid.vx+= dx*avoidObjExp*this.objectAvoidFactor
-                    boid.vy+= dy*avoidObjExp*this.objectAvoidFactor
-                    boid.vz+= dz*avoidObjExp*this.objectAvoidFactor
+                    boid.velocity.x+= dx*avoidObjExp*this.objectAvoidFactor
+                    boid.velocity.y+= dy*avoidObjExp*this.objectAvoidFactor
+                    boid.velocity.z+= dz*avoidObjExp*this.objectAvoidFactor
                 }
 
                 
@@ -213,21 +215,21 @@ export default class BoidLogic
                 // calculate boids speed
                 //NOTE can get rid of the sqrt, move this check to before each variable(environment -> seperation -> alignment -> cohesion) is added to 
                 //create heirachy with the fish
-                const speed = Math.sqrt(boid.vx**2+boid.vy**2+boid.vz**2)
+                const speed = Math.sqrt(boid.velocity.x**2+boid.velocity.y**2+boid.velocity.z**2)
 
                 //enforce speedlimits
 
                 if (speed< this.minSpeed)
                     {
-                        boid.vx= (boid.vx/speed)*this.minSpeed
-                        boid.vy= (boid.vy/speed)*this.minSpeed
-                        boid.vz= (boid.vz/speed)*this.minSpeed
+                        boid.velocity.x= (boid.velocity.x/speed)*this.minSpeed
+                        boid.velocity.y= (boid.velocity.y/speed)*this.minSpeed
+                        boid.velocity.z= (boid.velocity.z/speed)*this.minSpeed
                     }
                 if (speed> this.maxSpeed)
                     {
-                        boid.vx= (boid.vx/speed)*this.maxSpeed
-                        boid.vy= (boid.vy/speed)*this.maxSpeed
-                        boid.vz= (boid.vz/speed)*this.maxSpeed
+                        boid.velocity.x= (boid.velocity.x/speed)*this.maxSpeed
+                        boid.velocity.y= (boid.velocity.y/speed)*this.maxSpeed
+                        boid.velocity.z= (boid.velocity.z/speed)*this.maxSpeed
                     }
 
                     //NOTE: Math.sqrt is a slow algorithm. better to use a distance/speed squared check. But I am yet to see noticable performace increases. further testing needed
@@ -250,10 +252,18 @@ export default class BoidLogic
                     //         boid.vz= (boid.vz/speed)*this.maxSpeed
                     //     }
                 
+                
+                const currentPosition= boid.position.clone()
+
                 //update positions
-                boid.x+=boid.vx
-                boid.y+=boid.vy
-                boid.z+=boid.vz
+                boid.position.x+=boid.velocity.x
+                boid.position.y+=boid.velocity.y
+                boid.position.z+=boid.velocity.z
+
+                //update rotation
+                const m4=new THREE.Matrix4();
+                m4.lookAt(currentPosition,boid.position, new THREE.Vector3(0,1,0))
+                boid.rotationMatrix= m4 ;
 
         })
     }
@@ -299,32 +309,32 @@ export default class BoidLogic
     #solidWall(boid)
     {
 
-        if(this.boundingBox.max.y<boid.y)  //top
+        if(this.boundingBox.max.y<boid.position.y)  //top
             {
                 // console.log(this.boundingBox)
-                boid.vy-=this.turnFactor
+                boid.velocity.y-=this.turnFactor
             }
-        if(this.boundingBox.min.y>boid.y)  //bottom
+        if(this.boundingBox.min.y>boid.position.y)  //bottom
         {
-            boid.vy+=this.turnFactor
+            boid.velocity.y+=this.turnFactor
         }
         
-        if(this.boundingBox.max.x<boid.x)  //right
+        if(this.boundingBox.max.x<boid.position.x)  //right
             {
-                boid.vx-=this.turnFactor
+                boid.velocity.x-=this.turnFactor
             }
-        if(this.boundingBox.min.x>boid.x)  //left
+        if(this.boundingBox.min.x>boid.position.x)  //left
             {
-                boid.vx+=this.turnFactor
+                boid.velocity.x+=this.turnFactor
             }
         
-        if(this.boundingBox.max.z<boid.z)  //front
+        if(this.boundingBox.max.z<boid.position.z)  //front
             {
-                boid.vz-=this.turnFactor
+                boid.velocity.z-=this.turnFactor
             }
-        if(this.boundingBox.min.z>boid.z)  //back
+        if(this.boundingBox.min.z>boid.position.z)  //back
             {
-                boid.vz+=this.turnFactor
+                boid.velocity.z+=this.turnFactor
             }
 
         return boid
@@ -378,12 +388,9 @@ class Boid
 {
     constructor(x,y,z,vx,vy,vz)
     {
-        this.x=x
-        this.y=y
-        this.z=z
-        this.vx=vx
-        this.vy=vy
-        this.vz=vz
+        this.position=new THREE.Vector3(x,y,z)
+        this.velocity=new THREE.Vector3(vx,vy,vz)
+        this.rotationMatrix=new THREE.Matrix4()
         this.targetX=0
         this.targetY=0
         this.targetZ=0
